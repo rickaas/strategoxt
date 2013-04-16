81f745548ac473b00e5f082920c537d664c822f2
rickaas <rtlindeman@gmail.com>
2013-04-17 00:20:11 +0200

    include ant statistics logger to measure target duration

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
 LIBDSLDI="../../dist-libdsldi"
-ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib . -lib $LIBDSLDI "$@"
+LIBDSLDI_ARGS="-lib $LIBDSLDI"
+THIRDPARTY="../../thirdparty"
+LOGGING_ARGS="-lib $THIRDPARTY -logger de.pellepelster.ant.statistics.AntStatisticsLogger"
+
+ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib . $LIBDSLDI_ARGS $LOGGING_ARGS "$@"
