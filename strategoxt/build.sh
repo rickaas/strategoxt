57f09632b9d432d25028010b7971874badbeb419
rickaas <rtlindeman@gmail.com>
2013-04-17 23:44:34 +0200

    clean up the build scripts

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -1,4 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 
 LIBDSLDI="../../dist-libdsldi"
-ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib . -lib $LIBDSLDI "$@"
+THIRDPARTY="../../thirdparty"
+ANT_STATS_ARGS="-lib $THIRDPARTY -logger de.pellepelster.ant.statistics.AntStatisticsLogger"
+ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib . -lib $LIBDSLDI $ANT_STATS_ARGS "$@"
