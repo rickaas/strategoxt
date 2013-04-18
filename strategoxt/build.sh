fb5bce6a361f3f1eecb6788f4ae10ff029171ba6
rickaas <rtlindeman@gmail.com>
2013-04-19 00:09:34 +0200

    Increase memory

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -5,4 +5,4 @@ LIBDSLDI_ARGS="-lib $LIBDSLDI"
 THIRDPARTY="../../thirdparty"
 LOGGING_ARGS="-lib $THIRDPARTY -logger de.pellepelster.ant.statistics.AntStatisticsLogger"
 
-ANT_OPTS="-Xss8m -Xmx1024m -server -XX:+UseParallelGC -XX:MaxPermSize=256m" ant -lib . $LIBDSLDI_ARGS $LOGGING_ARGS "$@"
+ANT_OPTS="-Xss8m -Xmx2048m -server -XX:+UseParallelGC -XX:MaxPermSize=512m" ant -lib . $LIBDSLDI_ARGS $LOGGING_ARGS "$@"
