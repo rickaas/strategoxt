dd233ada7c59ad8ca57664a3474dfc7b8a7d43f4
rickaas <rtlindeman@gmail.com>
2013-04-19 10:29:50 +0200

    remote extra ant arguments to build script

--- strategoxt/build.sh
+++ strategoxt/build.sh
@@ -2,7 +2,5 @@
 
 LIBDSLDI="../../dist-libdsldi"
 LIBDSLDI_ARGS="-lib $LIBDSLDI"
-THIRDPARTY="../../thirdparty"
-LOGGING_ARGS="-lib $THIRDPARTY -logger de.pellepelster.ant.statistics.AntStatisticsLogger"
 
-ANT_OPTS="-Xss8m -Xmx2048m -server -XX:+UseParallelGC -XX:MaxPermSize=512m" ant -lib . $LIBDSLDI_ARGS $LOGGING_ARGS "$@"
+ANT_OPTS="-Xss8m -Xmx2048m -server -XX:+UseParallelGC -XX:MaxPermSize=512m" ant -lib . $LIBDSLDI_ARGS "$@"
